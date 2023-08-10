2ff7d658449ad9c6a9edfdf55dd458ea027e6ba3
GDC94 <germanderbescatoni@gmail.com>
2023-08-10 16:09:19 -0300

    add transfer from method

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const Tokenjson = artifacts.require("ERC20");
 const { expect } = require("chai");
 
 contract("test js", (accounts) => {
-  [alice, bob, owner, peter] = accounts;
+  [alice, bob, owner, peter, spender, recipient] = accounts;
 
   before(async () => {
     Token = await Tokenjson.new("German", "GMN", "18", "10000", {
@@ -10,23 +10,28 @@ contract("test js", (accounts) => {
     });
     // Aprobación inicial del propietario para que el spender gaste 500 tokens
     await Token.approve(alice, 500, { from: owner });
+    await Token.approve(spender, 500, { from: owner });
   });
 
   describe("Proofs into the constructor", async () => {
     it("The name of token should be equals to German", async () => {
-      const res = await Token.name();
-      expect(res).to.equal("German");
+      const nameOfToken = await Token.name();
+      expect(nameOfToken).to.equal("German");
+      expect(nameOfToken).to.not.equal("german");
     });
 
     it("The Symbol of token should be equal to GMN", async () => {
-      const res = await Token.symbol();
-      expect(res).to.equal("GMN");
+      const symbol = await Token.symbol();
+      expect(symbol).to.equal("GMN");
+      expect(symbol).to.not.equal("gmn");
     });
 
     it("The decimals of token should be equals to 18", async () => {
-      const res = await Token.decimals();
-      console.log(res);
+      const expectedDecimals = 18;
+      const actualDecimals = await Token.decimals();
+      expect(actualDecimals.toNumber()).to.equal(expectedDecimals);
     });
+
     it("The balance owner should be eql to 10000", async () => {
       const res = await Token.balanceOf(owner);
       const balanceOwner = "10000";
@@ -63,7 +68,7 @@ contract("test js", (accounts) => {
     });
   });
 
-  describe("transfer", async () => {
+  describe("Proofs in a Transfer method", async () => {
     it("transfer success", async () => {
       await Token.transfer(bob, "5000", {
         from: owner,
@@ -72,6 +77,28 @@ contract("test js", (accounts) => {
         await Token.balanceOf(owner)
       ).toString();
       expect(balanceOwnerAfterTransfer).to.equal("5000");
+    });
+  });
+
+  describe("Token", () => {
+    it("debería transferir tokens desde el spender al destinatario después de la aprobación", async () => {
+      const transferAmount = 100;
+      // El spender realiza la transferencia desde el owner al destinatario
+      const result = await Token.transferFrom(
+        owner,
+        recipient,
+        transferAmount,
+        { from: spender }
+      );
+      // Verifica que la transferencia sea exitosa
+      console.log(result.receipt);
+      expect(result.receipt.status).to.equal(true);
+
+      // Verifica que los balances se hayan actualizado correctamente
+      const ownerBalance = await Token.balanceOf(owner);
+      const recipientBalance = await Token.balanceOf(recipient);
+      expect(ownerBalance.toNumber()).to.equal(4900); // 10000 - 300
+      expect(recipientBalance.toNumber()).to.equal(100);
     });
   });
 });
